fd4fe0df891748676d47f425b06ab82a18bf9233
Sergio Pacheco <ch_e_ch_o@hotmail.com>
2021-07-08 22:20:33 -0400

    xlabes months

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import json
 import requests
-import sys
-import re
-
+import sys, re
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from PyQt5 import QtCore
-from PyQt5 import QtGui
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
 from PyQt5 import QtCore as qtc
 from dateutil import parser as date_parser
 from datetime import datetime, timedelta
-
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_qt5agg \
-    import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+#from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
 
 
 uifile_1 = 'UIfiles/menu.ui'
@@ -26,126 +21,68 @@ form_1, base_1 = uic.loadUiType(uifile_1)
 uifile_2 = 'UIfiles/grafico_por_mes.ui'
 form_2, base_2 = uic.loadUiType(uifile_2)
 
-global host
-host = "http://127.0.0.1:8007"
-
-
 class Menu(base_1, form_1):
-    def __init__(self, *args, **kwargs):
+    def __init__(self,*args, **kwargs):
         super(base_1, self).__init__(*args, **kwargs)
-        self.setupUi(self)
+        self.setupUi(self)  
 
-        # Eventos
+        #Eventos 
         self.clientesMes.clicked.connect(self.ClientesMes)
         self.clientesRegion.clicked.connect(self.ClientesRegion)
 
+    
     def ClientesMes(self):
-        self.cm = Clientes_mes()
+        self.cm= Clientes_mes()
         self.cm.show()
-
+    
     def ClientesRegion(self):
         self.cr = Clientes_region()
         self.cr.show()
 
-
 class Clientes_mes(base_2, form_2):
-    def __init__(self, *args, **kwargs):
+    def __init__(self,*args, **kwargs):
         super(base_2, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
-        months = ['Enero', 'Febrero', 'Marzo', 'Abril',
-                  'Mayo', 'Junio', 'Julio', 'Agosto',
-                  'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
-        # Example
-        agno = self.agno.value()
-        y1 = agno
-        y2 = agno
-        r = requests.get('{}/reservas_mes/{}/{}'.format(host, y1, y2))
-        r = r.json()['data']
-        count = [[0 for x in range(1, 13)] for i in range(y1, y2+1)]
-        for d in r:
-            count[d['year']-y1][d['month']-1] = d['count']
-        # print(count[0])
-        self.MplWidget.canvas.ax.bar(x=range(12),
-                                     height=[0]*12,
-                                     width=0.8, bottom=0,
-                                     align='center',
-                                     color="orange",
-                                     dgecolor=None,
-                                     linewidth=None,
-                                     err=None, yerr=None)
+        
+        #Example
+        self.MplWidget.canvas.ax.bar(x=range(12), height=[20, 10, 5, 3, 2, 1, 2, 3, 4, 5, 6, 2], width=0.8, bottom=0, align='center', color=None, edgecolor=None, linewidth=None, xerr=None, yerr=None)
         self.MplWidget.canvas.ax.set_ylabel('N° de clientes por mes')
         self.MplWidget.canvas.ax.set_xticks(range(12))
-        self.MplWidget.canvas.ax.set_xticklabels(months)
-        self.MplWidget.canvas.ax.clear()
+        self.MplWidget.canvas.ax.set_xticklabels(['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'])
 
-        self.MplWidget.canvas.ax.bar(x=range(12),
-                                     height=count[0],
-                                     color="orange")
-        self.MplWidget.canvas.draw()
+        #self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
 
-        # Eventos
+        #Eventos
         self.agno.valueChanged.connect(self.FiltrarPorAgno)
-
+   
     def FiltrarPorAgno(self):
         agno = self.agno.value()
-        y1 = agno
-        y2 = agno
-        # print(agno)
-        # print()
-        r = requests.get('{}/reservas_mes/{}/{}'.format(host, y1, y2))
-        r = r.json()['data']
-        count = [[0 for x in range(1, 13)] for i in range(y1, y2+1)]
-        for d in r:
-            count[d['year']-y1][d['month']-1] = d['count']
-        # print(count[0])
-        self.MplWidget.canvas.ax.clear()
-        self.MplWidget.canvas.ax.bar(x=range(12),
-                                     height=count[0],
-                                     color="orange")
-        self.MplWidget.canvas.draw()
 
-        # Consulta filtrar po año
-
+        #Consulta filtrar po año
 
 class Clientes_region(FigureCanvas):
     def __init__(self):
 
-        fig, self.ax = plt.subplots(figsize=(12, 6),
-                                    tight_layout=True,
-                                    facecolor='w')
+        fig, self.ax = plt.subplots(figsize=(12 , 6), tight_layout=True, facecolor='w')
         super().__init__(fig)
 
-        regiones = ["Tarapaca", "Antofagasta", "Atacama",
-                    "Coquimbo", "Valparaiso", "OHiggins",
-                    "Maule", "Biobio", "Araucania", "Lagos",
-                    "Campo", "Magallanes", "Metropolitana",
-                    "Rios", "Arica"]
-        resreg = [0] * 15
-        r = requests.get('{}/reservas_region'.format(host))
-        r = r.json()['data']
-
-        for d in r:
-            try:
-                resreg[regiones.index(d["Procedencia"])] = d["count"]
-            except ValueError:
-                pass
-
-        # Example
-        self.ax.bar(x=range(15), height=resreg, width=0.8,
-                    bottom=0, align='center', color=None,
-                    edgecolor=None, linewidth=None, xerr=None, yerr=None)
+        #Example
+        self.ax.bar(x=range(15), height=[20, 10, 5, 3, 4, 2, 6, 2, 1, 2, 6, 3, 8, 2, 1], width=0.8, bottom=0, align='center', color=None, edgecolor=None, linewidth=None, xerr=None, yerr=None)
         self.ax.set_ylabel('Clientes por región')
         self.ax.set_xticks(range(15))
-        self.ax.set_xticklabels(['I', 'II', 'III', 'IV', 'V', 'VI',
-                                 'VII', 'VIII', 'IX', 'X', 'XI',
-                                 'XII', 'XIII', 'XIV', 'XV'])
+        self.ax.set_xticklabels(['I', 'II', 'III','IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV'])
 
-        # Consulta clientes por region
-
+        #Consulta clientes por region
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Menu()
     ex.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_())   
+
+
+
+
+
+
--- mplwidget.py
+++ mplwidget.py
@@ -2,33 +2,31 @@
 # -------------------- mplwidget.py --------------------
 # ------------------------------------------------------
 from PyQt5.QtWidgets import*
+
 from matplotlib.backends.backend_qt5agg import FigureCanvas
+
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
-
+    
 class MplWidget(QWidget):
-
-    def __init__(self, parent=None):
+    
+    def __init__(self, parent = None):
 
         QWidget.__init__(self, parent)
+        #fig, self.ax = plt.subplots(figsize=(12, 6), tight_layout=True, facecolor='w')
+        #super().__init__(fig)
 
         self.canvas = FigureCanvas(Figure())
+        #self.canvas.fig, self.canvas.ax = plt.subplots(figsize=(12 , 6), tight_layout=True, facecolor='w')
         self.canvas.ax = self.canvas.figure.add_subplot(111)
         vertical_layout = QVBoxLayout()
         vertical_layout.addWidget(self.canvas)
 
-        self.canvas.ax.bar(x=range(12), height=[20, 10, 5, 3, 2, 1, 2,
-                                                3, 4, 5, 6, 2],
-                           width=0.8, bottom=0, align='center',
-                           color=None, edgecolor=None, linewidth=None,
-                           xerr=None, yerr=None)
+        self.canvas.ax.bar(x=range(12), height=[20, 10, 5, 3, 2, 1, 2, 3, 4, 5, 6, 2], width=0.8, bottom=0, align='center', color=None, edgecolor=None, linewidth=None, xerr=None, yerr=None)
         self.canvas.ax.set_ylabel('Infectados totales a la fecha')
         self.canvas.ax.set_xticks(range(12))
-        self.canvas.ax.set_xticklabels(['Enero', 'Febrero', 'Marzo',
-                                        'Abril', 'Mayo', 'Junio',
-                                        'Julio', 'Agosto', 'Septiembre',
-                                        'Octubre', 'Noviembre',
-                                        'Diciembre'])
-
+        self.canvas.ax.set_xticklabels(['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'])
+        
         self.setLayout(vertical_layout)
+
